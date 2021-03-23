51135dce7036f150933fc0da4100a9c6ae447ebf
robsevcik <robert.a.sevcik@gmail.com>
2021-03-22 17:41:30 -0700

    Delete people_dictionary.py

--- people_dictionary.py
+++
@@ -1,6 +0,0 @@
-people_dictionary = {'Brett' : ['Male' , 'Weight 175'],
-                     'Nancy' : ['Female' , 'weight 125'],
-                     'Patrick' : ['Male' , 'weight 195'],
-                     'Briar' : ['female' , 'weight 115'],
-                     'Adam' : ['Male' , 'weight 215']}
-print(people_dictionary)
